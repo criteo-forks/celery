fd707a1635f4952c92c718033c8ea412d0adea6c
Clement Michaud <c.michaud@criteo.com>
2020-03-18 11:47:52 +0100

    Bump to v4.3.0+criteo15

--- celery/__init__.py
+++ celery/__init__.py
@@ -14,7 +14,7 @@ from collections import namedtuple
 
 SERIES = 'rhubarb'
 
-__version__ = '4.3.0+criteo14'
+__version__ = '4.3.0+criteo15'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://celeryproject.org'
